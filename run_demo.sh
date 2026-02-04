e5fff18c4559c96dcbfd0c433bc9bde3cdcc9016
alex27182818284590452353602874713526624 <153064103+alex27182818284590452353602874713526624@users.noreply.github.com>
2026-02-04 09:54:15 +0000

    ...

--- run_demo.sh
+++ run_demo.sh
@@ -6,6 +6,7 @@ REPO_DIR="${HOME}/inventar"
 rm -rf "$REPO_DIR"
 git clone --recurse-submodules https://github.com/YukiYamazaki47/inventar.git "$REPO_DIR"
 cd "$REPO_DIR"
+git checkout aws
 
 echo "Starting Docker containers..."
 docker-compose up -d
